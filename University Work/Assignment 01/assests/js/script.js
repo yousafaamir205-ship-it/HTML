e5e8b210923cc91d045152222315d95c4d1ec14d
MUHAMMAD YOUSAF <yousafaamir205@gamil.com>
2026-09-14 21:24:41 +0500

    makinG Navbar

---
+++ University Work/Assignment 01/assests/js/script.js
@@ -0,0 +1,4 @@
+const menuButton = document.getElementById("menu-button");
+const navMenu = document.getElementById("nav-menu");
+menuButton.addEventListener("click",()=>{
+navMenu.classList.toggle("hidden");});
