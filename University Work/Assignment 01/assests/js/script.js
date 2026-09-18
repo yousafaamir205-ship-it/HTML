700a1bb9945c8d122137c9af95e40b36007354ce
MUHAMMAD YOUSAF <yousafaamir205@gmail.com>
2026-09-18 11:44:30 +0500

    Delete University Work/Assignment 01 directory

--- University Work/Assignment 01/assests/js/script.js
+++
@@ -1,4 +0,0 @@
-const menuButton = document.getElementById("menu-button");
-const navMenu = document.getElementById("nav-menu");
-menuButton.addEventListener("click",()=>{
-navMenu.classList.toggle("hidden");});
